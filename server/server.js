6503062e46a744a9235a270157fd65fcc2d4e21b
dhruvin7k9 <dhruvink79@gmail.com>
2024-06-18 02:05:15 +0530

    server minor cookie error fix

--- server/server.js
+++ server/server.js
@@ -35,7 +35,6 @@ const io = socketIo(server, {
 app.use(bodyParser.json({limit: "50mb"}))
 app.use(bodyParser.urlencoded({ extended : true, limit: "50mb"}))
 app.use(express.json())
-app.use(cookieParser());
 
 // Headers
 app.use((req, res, next) => {
